c2dd8454ba4fc3499d541451dfc9ef0ed4da2fd2
Mark <mark@steadman.io>
2015-04-04 19:18:09 +0100

    Got ready for Django 1.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 
 setup(
 	name = 'bambu-markup',
-	version = '2.0',
+	version = '3.0',
 	description = 'Replacements for Django\'s Markdown and reStructuredText support',
 	author = 'Steadman',
 	author_email = 'mark@steadman.io',
